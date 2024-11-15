3dfb7853ba8d5864af19201589d8eb9e16b55460
rafaeltws <159142263+rafaeltws@users.noreply.github.com>
2024-11-15 20:18:36 -0300

    Add files via upload

---
+++ src/routes/Home.tsx
@@ -0,0 +1,35 @@
+import Search from "../components/Search"
+import { useState } from "react"
+import { UserProps } from "../types/user"
+import User from '../components/User'
+
+const Home = () => {
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    const loadUser = async(userName: string) => {
+        const res = await fetch(`https://api.github.com/users/${userName}`);
+
+        const data = await res.json()
+
+        const { avatar_url, login, location, followers, following } = data;
+
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following,
+          };
+
+          setUser(userData);
+    }
+
+    return (
+        <div>
+            <Search loadUser={loadUser} />
+            {user && <User {...user} />}
+        </div>
+    )
+}
+
+export default Home
